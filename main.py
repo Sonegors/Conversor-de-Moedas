34b59ac7234c8bbc3999e7f035877b99f6310afe
Juliano Sonego <98494302+Sonegors@users.noreply.github.com>
2023-01-09 19:46:39 +0000

    Add Ex: USD,EUR,BRL

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import httpx
 
-base_currency = input('Digite a moeda de base:').upper()
-currency = input('Digite a moeda desejada:').upper()
+base_currency = input("Digite a sigla da moeda que deseja converter (ex: BRL, USD, EUR): ").upper()
+currency = input("Insira a moeda que deseja a conversão (símbolo): ").upper()
 
 #Buscar valores da moedas em API publica
 
 response = httpx.get(
-  url=f'https://api.exchangerate.host/latest?base={base_currency}').json()
+    url=f'https://api.exchangerate.host/latest?base={base_currency}'
+).json()
 currency_data = response['rates']
 
 print(f'1 {base_currency} vale {currency_data.get(currency)} {currency}')
